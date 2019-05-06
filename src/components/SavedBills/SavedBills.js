706c8ea153c751408b817eb1d71f2f8326c4b19d
David Yu <davidpyu@gmail.com>
2019-05-06 10:33:30 -0500

    SavedBill check if atleast one bill has been saved.

--- src/components/SavedBills/SavedBills.js
+++ src/components/SavedBills/SavedBills.js
@@ -28,77 +28,85 @@ const savedBills = props => {
         setPanelOpen(expanded ? panel : false);
     };
 
-    const billsKeys = Object.keys(props.bills);
-    const billsValues = Object.values(props.bills);
+    // Generate lists only if existing bills have been saved
+    let billsPanels = "";
+    if (props.bills) {
+        const billsKeys = Object.keys(props.bills);
+        const billsValues = Object.values(props.bills);
 
-    let billsPanels = billsValues.map((value, index, arr) => {
-        let personsListKeys = Object.keys(billsValues[index].persons);
-        let personsListValues = Object.values(billsValues[index].persons);
-        let dollarPerPerson = props.convertTwoDecimal(billsValues[index].billAmount / personsListKeys.length);
-        let billId = billsKeys[index];
+        billsPanels = billsValues.map((value, index, arr) => {
+            let personsListKeys = Object.keys(billsValues[index].persons);
+            let personsListValues = Object.values(billsValues[index].persons);
+            let dollarPerPerson = props.convertTwoDecimal(billsValues[index].billAmount / personsListKeys.length);
+            let billId = billsKeys[index];
 
-        let displayPersonsList = personsListKeys.map((value, index) => {
-            return (
-                <ListItem key={"test" + index}>
-                    <ListItemText
-                        primary={personsListKeys[index]}
-                        secondary={personsListValues[index] === 1 ? "paid" : "NOT paid"}
-                        secondaryTypographyProps={personsListValues[index] === 1 ? {color: "textSecondary"} : {style: {color: "red"}}}
-                    />
-                    <ListItemSecondaryAction
-                        onClick={() => {
-                            console.log("clicked");
-                        }}>
-                        <Checkbox
-                            id={+"+" + personsListKeys[index]}
-                            onChange={event => {
-                                props.updatePaidStatus(event, billId, personsListKeys[index]);
-                            }}
-                            checked={personsListValues[index] === 1 ? true : false}
+            let displayPersonsList = personsListKeys.map((value, index) => {
+                return (
+                    <ListItem key={"test" + index}>
+                        <ListItemText
+                            primary={personsListKeys[index]}
+                            secondary={personsListValues[index] === 1 ? "paid" : "NOT paid"}
+                            secondaryTypographyProps={
+                                personsListValues[index] === 1
+                                    ? { color: "textSecondary" }
+                                    : { style: { color: "red" } }
+                            }
                         />
-                    </ListItemSecondaryAction>
-                </ListItem>
+                        <ListItemSecondaryAction
+                            onClick={() => {
+                                console.log("clicked");
+                            }}>
+                            <Checkbox
+                                id={+"+" + personsListKeys[index]}
+                                onChange={event => {
+                                    props.updatePaidStatus(event, billId, personsListKeys[index]);
+                                }}
+                                checked={personsListValues[index] === 1 ? true : false}
+                            />
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                );
+            });
+            return (
+                <ExpansionPanel
+                    key={"moretest" + index}
+                    expanded={panelOpen === "panel" + index}
+                    onChange={handleChange("panel" + index)}>
+                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                        <Typography className={classes.heading}>{billsValues[index].billName}</Typography>
+                        <Typography className={classes.secondaryHeading}>
+                            {formatDate(billsValues[index].billDate)}
+                        </Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                        <Grid container direction="row" alignContent="flex-start" alignItems="center">
+                            <Grid item xs={12} style={{ textAlign: "center" }}>
+                                <Typography inline color="textPrimary">
+                                    Total: ${billsValues[index].billAmount}
+                                </Typography>
+                                <Typography inline color="textSecondary" style={{ marginLeft: "0.5rem" }}>
+                                    (${dollarPerPerson} per person)
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <List dense className={classes.saved_bill_list_item}>
+                                    {displayPersonsList}
+                                </List>
+                            </Grid>
+                            <Grid item xs={12} style={{ textAlign: "center" }}>
+                                <Button
+                                    style={{ textAlign: "center" }}
+                                    color="secondary"
+                                    onClick={event => props.deleteBill(event, billsKeys[index])}>
+                                    delete
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
             );
         });
-
-        return (
-            <ExpansionPanel
-                key={"moretest" + index}
-                expanded={panelOpen === "panel" + index}
-                onChange={handleChange("panel" + index)}>
-                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography className={classes.heading}>{billsValues[index].billName}</Typography>
-                    <Typography className={classes.secondaryHeading}>{formatDate(billsValues[index].billDate)}</Typography>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails>
-                    <Grid container direction="row" alignContent="flex-start" alignItems="center">
-                        <Grid item xs={12} style={{ textAlign: "center" }}>
-                            <Typography inline color="textPrimary">
-                                Total: ${billsValues[index].billAmount}
-                            </Typography>
-                            <Typography inline color="textSecondary" style={{marginLeft: "0.5rem"}}>
-                                (${dollarPerPerson} per person)
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <List dense className={classes.saved_bill_list_item}>
-                                {displayPersonsList}
-                            </List>
-                        </Grid>
-                        <Grid item xs={12} style={{ textAlign: "center" }}>
-                            <Button
-                                style={{ textAlign: "center" }}
-                                color="secondary"
-                                onClick={event => props.deleteBill(event, billsKeys[index])}>
-                                delete
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </ExpansionPanelDetails>
-            </ExpansionPanel>
-        );
-    });
-
+    }
     return (
         <Grid item xs={12} key={"persons"}>
             {billsPanels}
